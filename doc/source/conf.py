363cdc269f4000ecbd8234d19f886861aff08686
André Ferrari <andferrari@gmail.com>
2015-03-10 23:10:52 +0100

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Painter.jl'
-copyright = u'2015, PAINTER'
+copyright = u'2015, Painter.jl'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -153,7 +153,7 @@ html_static_path = ['_static']
 #html_show_sphinx = True
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-#html_show_copyright = True
+html_show_copyright = False
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
