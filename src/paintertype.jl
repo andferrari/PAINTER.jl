c96d1d1dee56e846fafa3db743c14bd5b8c903cc
antony <antonyschutz@gmail.com>
2015-05-30 23:14:55 +0200

    separated phases prox

--- src/paintertype.jl
+++ src/paintertype.jl
@@ -38,6 +38,7 @@ type PAINTER_Input
     rho_ps::Real
     alpha::Real
     beta::Real
+    gamma::Real
     eps1::Real
     eps2::Real
     epsilon::Real
@@ -57,6 +58,8 @@ type PAINTER_Data
     plan::Array
     F3D::Array
     H::SparseMatrixCSC
+    HDP::SparseMatrixCSC
+    HT3::SparseMatrixCSC
     M::Array
     x::SharedArray{Float64}
     vHt::Array
@@ -74,9 +77,13 @@ type PAINTER_Data
     tau_xc::Array
     tau_pwc::Array
     tau_xic::Array
+    tau_xicdp::Array
+    tau_xict3::Array
     yc::Array
     y_v2::Array
     y_phi::Array
+    y_phidp::Array
+    y_phit3::Array
     crit1::Array
     crit2::Array
     ind::Int
@@ -104,11 +111,11 @@ end
 function painterinputinit()
     return PAINTER_Input(painterplotfct, "", [], [], [], [], [], [], []
                          , [], [], [], [], 0., 0., 0., 0., 0., 0., 0., 0.
-                         , 0., 0., 0., 0., 0., 0., 0., 0., [], [], []
+                         , 0., 0., 0., 0., 0., 0., 0., 0., 0., [], [], []
                          , true, [], [], [], [])
 end
 function painterdatainit()
-    return PAINTER_Data(0., [], [], speye(0), [], [], [], [], [], [], []
+    return PAINTER_Data(0., [], [], speye(0), speye(0), speye(0), [], [], [], [], [], [], []
                         , [], [], [], [], [], [], [], [], [], [], [], []
-                        , [], Float64[], Float64[], 0, 0, 0)
+                        , [], [], [], [], [], Float64[], Float64[], 0, 0, 0)
 end
