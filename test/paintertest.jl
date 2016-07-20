1c1fa6acfae50243e70317471f42ccb13905da35
antony <antonyschutz@gmail.com>
2016-07-20 09:49:52 +0200

    Paintertest.jl update
    - No OPTOPT Structure in output
    - Adjust result

--- test/paintertest.jl
+++ test/paintertest.jl
@@ -1,32 +1,28 @@
-using PAINTER
+# using PAINTER
 using Base.Test
 
-MyFOV = 0.01
-Myindwvl = 1:30
-Mynx = 64
-Myeps1 = 1e-4
-Myeps2 = 1e-4
-Myrho_y = 10.
-Myalpha = 1e4
-Mybeta = 1e5
-Myrho_spat = 4.
-Myrho_ps = Myrho_spat
-Mylambda_spat = 1e-5
-Myrho_spec = 0.5
-Mylambda_spec = 1e-5
-Mynbitermax = 4
+FOV = 0.01
+indwvl = 1:30
+nx = 64
+eps1 = 1e-4
+eps2 = 1e-4
+rho_y = 10.
+alpha = 1e4
+beta = 1e5
+rho_spat = 4.
+rho_ps = rho_spat
+lambda_spat = 1e-5
+rho_spec = 0.5
+lambda_spec = 1e-5
+nbitermax = 4
 dptype = "sliding"
 dpprm = 5
 
-OIDATA, PDATA, OPTOPT = painter(nbitermax = Mynbitermax, nx = Mynx, lambda_spat = Mylambda_spat,
-                            lambda_spec = Mylambda_spec, rho_y = Myrho_y, rho_spat = Myrho_spat,
-                            rho_spec = Myrho_spec, rho_ps = Myrho_ps, alpha = Myalpha, beta = Mybeta,
-                            eps1 = Myeps1, eps2 = Myeps2, FOV = MyFOV, indwvl = Myindwvl,
-                            dptype = dptype, dpprm = dpprm,
-                            ls = OptimPack.MoreThuenteLineSearch(ftol = 1e-8, gtol = 0.95),
-                            scl = OptimPack.SCALING_OREN_SPEDICATO, gat = 0, grt = 1e-3,
-                            vt = false, memsize = 100, mxvl = 1000, mxtr = 1000, stpmn = 1e-20,
-                            stpmx = 1e+20);
+OIDATA, PDATA = painter(nbitermax = nbitermax, nx = nx, lambda_spat = lambda_spat,
+                            lambda_spec = lambda_spec, rho_y = rho_y, rho_spat = rho_spat,
+                            rho_spec = rho_spec, rho_ps = rho_ps, alpha = alpha, beta = beta,
+                            eps1 = eps1, eps2 = eps2, FOV = FOV, indwvl = indwvl,
+                            dptype = dptype, dpprm = dpprm)
 
 
-@test_approx_eq_eps PDATA.crit1[1] 1.92813 1E-4
+@test_approx_eq_eps PDATA.crit1[1] 1.92717 1E-4
